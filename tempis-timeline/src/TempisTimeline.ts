cfd8dae091ce2e684220bfe235ffab7b43c4e4a0
Nikolas Howard <nikolas.howard@infor.com>
2025-02-21 08:51:00 +0000

    Started on item styling

--- tempis-timeline/src/TempisTimeline.ts
+++ tempis-timeline/src/TempisTimeline.ts
@@ -33,6 +33,7 @@ export class TempisTimeline {
         this._rangeView = new TimelineRangeView(this._canvas, this._options.range);
         this._dataSet = new TimelineDataSet(() => this._onDataSetChange());
         this._dataView = new TimelineDataView(this._dataSet);
+        // TODO Update to take this._options.style?.font.
         this._font = new TimelineFont(this._options.font);
 
         // Create our initial item groupings.
